585a5a5132b78e17f004d03fbe317aeb5e5ba7ea
VlaDexa <vgrechannik@gmail.com>
2023-11-04 22:52:11 +0100

    Fix up startup sequence

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,13 +1,13 @@
 import { env } from "$env/dynamic/private";
 import web_push from "web-push";
-const {setVapidDetails} = web_push;
-import { Pool, createPool, sql } from "@vercel/postgres";
+const { setVapidDetails } = web_push;
+import { createPool } from "@vercel/postgres";
 
 setVapidDetails("mailto:vgrechannik@gmail.com", env.VAPID_PUBLIC_KEY, env.VAPID_PRIVATE_KEY)
 export const pool = createPool({
-connectionString: env.POSTGRES_URL,
+	connectionString: env.POSTGRES_URL,
 });
-pool.sql`CREATE TABLE IF NOT EXISTS requests (
+await pool.sql`CREATE TABLE IF NOT EXISTS requests (
     id SERIAL PRIMARY KEY,
     endpoint VARCHAR(255) NOT NULL,
     expiration_time INT,

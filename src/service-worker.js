a39b954a31b6782e0ae8c1c54cc1001a0c0c33ac
VlaDexa <vgrechannik@gmail.com>
2023-11-04 22:02:21 +0100

    Actually paste the content

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,10 +1,12 @@
 // Register event listener for the 'push' event.
-self.addEventListener('push', function(event) {
+self.addEventListener('push', 
+/** @param {PushEvent} event*/
+  function(event) {
   // Keep the service worker alive until the notification is created.
   event.waitUntil(
     // Show a notification with title 'ServiceWorker Cookbook' and body 'Alea iacta est'.
     self.registration.showNotification('ServiceWorker Cookbook', {
-      body: 'Alea iacta est',
+      body: event.data.text(),
     })
   );
 });

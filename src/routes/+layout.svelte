ffa654c7ec263b7e0a0cdb1ffceb5bd08640cda7
VlaDexa <vgrechannik@gmail.com>
2023-11-06 02:25:29 +0100

    Rewrite service worker a bit to support older browsers

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,5 @@
-<script>
-	//import { browser } from '$app/environment';
-	//import { page } from '$app/stores';
-	//import { webVitals } from '$lib/vitals';
-	import './styles.css';
+<script lang="ts">
 	import '../app.css';
-
-	/** @type {import('./$types').LayoutServerData} */
-	export let data;
-
-	//$: if (browser && data?.analyticsId) {
-	//	webVitals({
-	//		path: $page.url.pathname,
-	//		params: $page.params,
-	//		analyticsId: data.analyticsId
-	//	});
-	//}
 </script>
 
 <main class="w-screen h-screen">
--- src/service-worker.js
+++ src/service-worker.js
@@ -1,12 +1,13 @@
 // Register event listener for the 'push' event.
-self.addEventListener('push', 
-/** @param {PushEvent} event*/
+self.addEventListener('push',
+  /** @param {PushEvent} event*/
   function(event) {
-  // Keep the service worker alive until the notification is created.
-  event.waitUntil(
-    // Show a notification with title 'ServiceWorker Cookbook' and body 'Alea iacta est'.
-    self.registration.showNotification('ServiceWorker Cookbook', {
-      body: event.data.text(),
-    })
-  );
-});
+    if (!(self.Notification && self.Notification.permission === "granted")) {
+      return;
+    }
+    const message = event.data.text();
+    new self.Notification(message, {
+      body: message,
+      tag: "server-message",
+    });
+  });

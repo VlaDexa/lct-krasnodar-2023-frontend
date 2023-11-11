3089228aa45732e3877ae794cbe848b352ee6484
VlaDexa <vgrechannik@gmail.com>
2023-11-11 15:01:13 +0100

    Send actual responses this time

--- src/routes/api/sendNotificationToUser/+server.ts
+++ src/routes/api/sendNotificationToUser/+server.ts
@@ -3,7 +3,7 @@ import type { RequestHandler } from './$types';
 import type { QueryResultRow } from '@vercel/postgres';
 import type { PushSubscriptionModel } from '../../../models';
 import { pool } from '../../../hooks.server';
-import { fail } from '@sveltejs/kit';
+import { fail, json, text } from '@sveltejs/kit';
 import web_push from 'web-push';
 
 const Notification = z.object({
@@ -23,10 +23,10 @@ export type Notifications = z.infer<typeof Notification>;
 
 export const POST: RequestHandler = async ({ request }) => {
 	if (request.headers.get('Content-Type') === 'application/json')
-		throw fail(415, { message: 'Wrong MIME type!' });
+		return json({ message: 'Wrong MIME type!' }, { status: 415 });
 	const data = await request.json();
 	const parsed = Notification.safeParse(data);
-	if (!parsed.success) throw fail(422, { message: parsed.error.message });
+	if (!parsed.success) return json({ message: parsed.error.message }, { status: 422 });
 	const notification = parsed.data;
 	const allPush = await pool.sql<
 		QueryResultRow & PushSubscriptionModel & { id: number }

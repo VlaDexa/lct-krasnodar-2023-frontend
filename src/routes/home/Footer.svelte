5e19c87cf94503635c1d9a310b0c47eddc464214
VlaDexa <vgrechannik@gmail.com>
2023-11-11 17:04:49 +0100

    Adaptation page

--- src/routes/home/Footer.svelte
+++ src/routes/home/Footer.svelte
@@ -2,7 +2,7 @@
 	import mail from '$lib/images/mail.svg';
 </script>
 
-<footer class="border-t border-[#98A2B3] py-2 px-6 lg:px-5 xl:px-[100px] 2xl:px-[200px]">
+<footer style:view-transition-name="footer" class="border-t border-[#98A2B3] py-2 px-6 lg:px-5 xl:px-[100px] 2xl:px-[200px]">
 	<a href="https://google.com" class="text-[#667085] items-center gap-1 flex text-sm lg:text-[28px]">
 		<img class="lg:w-10" src={mail} alt="" />
 		Техническая поддержка
---
+++ src/routes/home/adaptation/+page.svelte
@@ -0,0 +1,118 @@
+<script>
+	import secHeader from '$lib/images/sec-header.svg';
+	import clock from '$lib/images/clock.svg';
+	import leafBlue from '$lib/images/leaf-blue.svg';
+	import xpBlue from '$lib/images/xp-blue.svg';
+	import arrowShort from '$lib/images/arrow-short.svg';
+	import bigSec from '$lib/images/big-sec.svg';
+	import xClose from '$lib/images/x-close.svg';
+	import bigSecText from '$lib/images/big-sec-text.svg';
+
+	let closed = false;
+</script>
+
+<div class="grid grid-cols-3 gap-14 py-10 lg:px-5 xl:px-[100px] 2xl:px-[200px]">
+	<section class="flex flex-col">
+		<img src={secHeader} alt="" />
+		<div class="px-7 py-5">
+			<h2>Вся информация о компании</h2>
+			<p class="awards">Награды</p>
+			<ul class="mt-5 flex flex-row gap-5">
+				<li><img src={leafBlue} alt="Первые побеги" /></li>
+				<li><img src={xpBlue} alt="Максимум опыта" /></li>
+			</ul>
+			<p class="closest">Ближайший срок</p>
+			<div class="mt-4 flex flex-row justify-between">
+				<div class="deadline flex flex-row">
+					<img src={clock} alt="" />
+					<p>22 ноября</p>
+					<p>23:59</p>
+				</div>
+				<button class="arrow"><img src={arrowShort} alt="" /></button>
+			</div>
+		</div>
+	</section>
+	{#if !closed}
+		<section class="col-span-2 flex">
+			<img src={bigSec} alt="" class="h-full" />
+			<div class="flex grow flex-col pt-9 pr-[30px]">
+				<div class="flex flex-row justify-between">
+					<h2>Награды и опыт</h2>
+					<button> <img src={xClose} alt="Close" /> </button>
+				</div>
+				<div class="grow">
+					<img class="h-full w-full" src={bigSecText} alt="Just close it" />
+				</div>
+			</div>
+		</section>
+	{/if}
+	<section class="h-[441px]"></section>
+	<section></section>
+	<section></section>
+</div>
+
+<style>
+	section {
+		box-shadow:
+			0px 1px 2px 0px rgba(79, 79, 79, 0.1),
+			0px 3px 3px 0px rgba(79, 79, 79, 0.09),
+			0px 6px 4px 0px rgba(79, 79, 79, 0.05),
+			0px 11px 4px 0px rgba(79, 79, 79, 0.01),
+			0px 17px 5px 0px rgba(79, 79, 79, 0);
+		background-color: white;
+		border-radius: 16px;
+	}
+
+	.awards {
+		color: var(--gray-7-aa, #667085);
+		font-family: Inter;
+		font-size: 24px;
+		font-style: normal;
+		font-weight: 400;
+		line-height: normal;
+		margin-top: 24px;
+	}
+	h2 {
+		color: var(--gray-8-aaa, #475467);
+		font-family: Inter;
+		font-size: 36px;
+		font-style: normal;
+		font-weight: 500;
+		line-height: normal;
+	}
+
+	.closest {
+		color: var(--gray-7-aa, #667085);
+		font-family: Inter;
+		font-size: 24px;
+		font-style: normal;
+		font-weight: 400;
+		line-height: normal;
+	}
+
+	.deadline {
+		border-radius: 12px;
+		background: var(--main-2, #eff8ff);
+		display: flex;
+		align-items: center;
+		gap: 6px;
+		color: var(--gray-8-aaa, #475467);
+		font-family: Inter;
+		font-size: 24px;
+		font-style: normal;
+		font-weight: 400;
+		line-height: normal;
+	}
+
+	.arrow {
+		border-radius: 16px;
+		background: var(--main-8-aa, #1570ef);
+		box-shadow:
+			0px 2px 4px 0px rgba(79, 79, 79, 0.1),
+			0px 6px 6px 0px rgba(79, 79, 79, 0.09),
+			0px 12px 8px 0px rgba(79, 79, 79, 0.05),
+			0px 22px 8px 0px rgba(79, 79, 79, 0.01),
+			0px 34px 10px 0px rgba(79, 79, 79, 0);
+		padding: 8px 20px;
+	}
+</style>

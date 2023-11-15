931e1bcf9ff10c801c0604ca115975c56cacb59a
VlaDexa <vgrechannik@gmail.com>
2023-11-15 21:38:20 +0100

    Switch to edge functions

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -3,13 +3,6 @@ import { redirect, fail } from '@sveltejs/kit';
 import { ApiError, UserService } from '$lib/openapi';
 import type { Config } from "@sveltejs/adapter-vercel";
 
-export const config: Config = {
-	isr: {
-		expiration: 60,
-		allowQuery: [],
-	}
-};
-
 export const actions: Actions = {
 	default: async ({ cookies, request }) => {
 		const form = await request.formData();

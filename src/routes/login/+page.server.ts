f7bf108180f03a1327f086b4ec761978333725ae
VlaDexa <vgrechannik@gmail.com>
2023-11-15 23:23:06 +0100

    Revert "Yeah, just one route at a time"
    This reverts commit 2db51ad94ce1ca7d12b2b4fafc911838b0c85ef7.

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,11 +1,7 @@
 import type { Actions } from './$types';
 import { redirect, fail } from '@sveltejs/kit';
 import { ApiError, UserService } from '$lib/openapi';
-import type { Config } from '@sveltejs/adapter-vercel';
-
-export const config: Config = {
-	runtime: "edge"
-}
+import type { Config } from "@sveltejs/adapter-vercel";
 
 export const actions: Actions = {
 	default: async ({ cookies, request }) => {
